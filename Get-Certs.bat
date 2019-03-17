1b0ce6ec83b5576d90582d9cd945a81456cd713e
Muffinman99991 <39826286+Muffinman99991@users.noreply.github.com>
2019-03-17 21:26:25 +0100

    Kommentar ins Batch File eingefügt

--- Get-Certs.bat
+++ Get-Certs.bat
@@ -1,3 +1,15 @@
+::Für dieses Batchfile ist Putty.exe bzw. pscp.exe zwingend notwendig!
+::
+::
+::Das Root Passwort, die Server IP, der remote Pfad und der lokale Pfad sollte gegebenfalls geändert werden.
+::pscp.exe -pw <root passwort> root@<Server-IP>:<Pfad auf Server> <Lokaler Pfad>
+::
+::
+::Befindet sich ein Leerschlag im Windows Pfad, so muss aus dem Pfad ein String gemacht werden (<"Pfad">)
+::
+::Möchte das Passwort nicht in diesem File eingegeben werden, so kann der Parameter <-pw> weggelassen werden und man wird beim Ausführen 
+::des Befehles 3x nach dem Root PW gefragt.
+::
 pscp.exe -pw Miau123 root@192.168.33.10:/etc/openvpn/client.key "C:\Program Files\OpenVPN\config"
 pscp.exe -pw Miau123 root@192.168.33.10:/etc/openvpn/client.crt "C:\Program Files\OpenVPN\config"
 pscp.exe -pw Miau123 root@192.168.33.10:/etc/openvpn/ca.crt "C:\Program Files\OpenVPN\config"
