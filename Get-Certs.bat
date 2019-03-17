ec3a43528276e403b6454071f8102d2a3ca7598a
Muffinman99991 <39826286+Muffinman99991@users.noreply.github.com>
2019-03-17 17:32:33 +0100

    Add files via upload

---
+++ Get-Certs.bat
@@ -0,0 +1,3 @@
+pscp.exe -pw Miau123 root@192.168.33.10:/etc/openvpn/client.key "C:\Program Files\OpenVPN\config"
+pscp.exe -pw Miau123 root@192.168.33.10:/etc/openvpn/client.crt "C:\Program Files\OpenVPN\config"
+pscp.exe -pw Miau123 root@192.168.33.10:/etc/openvpn/ca.crt "C:\Program Files\OpenVPN\config"
