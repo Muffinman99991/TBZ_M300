a3ed34dfabd64cb7d16b5e1e63dbd84bea06fcec
Muffinman99991 <39826286+Muffinman99991@users.noreply.github.com>
2019-03-19 18:43:19 +0100

    Apache SSL hinzugefügt

--- bootstrap.sh
+++ bootstrap.sh
@@ -120,6 +120,20 @@ systemctl start openvpn@server
 #Bei Fehlermedlung Status checken:
 #systemctl status openvpn@server
 
+##################APACHE SSL KONFIGURIEREN##############################
+a2enmod ssl
+service apache2 restart
+
+mkdir /etc/apache2/ssl
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/apache2/ssl/apache.key -out /etc/apache2/ssl/apache.crt --batch
+
+sed -i s/'\/etc\/ssl\/certs\/ssl-cert-snakeoil.pem'/'\/etc\/apache2\/ssl\/apache.crt'/g /etc/apache2/sites-available/default-ssl.conf
+sed -i s/'\/etc\/ssl\/private\/ssl-cert-snakeoil.key'/'\/etc\/apache2\/ssl\/apache.key'/g /etc/apache2/sites-available/default-ssl.conf
+
+a2ensite default-ssl.conf
+
+service apache2 restart
+
 
 
 
