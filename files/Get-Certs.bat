e1f6cedc411ef303257077fe4dd3283fa9fe8843
Muffinman99991 <39826286+Muffinman99991@users.noreply.github.com>
2019-03-22 14:00:23 +0100

    Letzte Änderung

--- files/Get-Certs.bat
+++ files/Get-Certs.bat
@@ -1,4 +1,4 @@
-pscp.exe -pw Miau123 root@192.168.192.36:/etc/openvpn/client/client.key "C:\Program Files\OpenVPN\config"
-pscp.exe -pw Miau123 root@192.168.192.36:/etc/openvpn/client/client.crt "C:\Program Files\OpenVPN\config"
-pscp.exe -pw Miau123 root@192.168.192.36:/etc/openvpn/ca.crt "C:\Program Files\OpenVPN\config"
-pscp.exe -pw Miau123 root@192.168.192.36:/etc/openvpn/server/ta.key "C:\Program Files\OpenVPN\config"
+pscp.exe -pw Miau123 root@10.71.13.4:/etc/openvpn/client/client.key "C:\Program Files\OpenVPN\config"
+pscp.exe -pw Miau123 root@10.71.13.4:/etc/openvpn/client/client.crt "C:\Program Files\OpenVPN\config"
+pscp.exe -pw Miau123 root@10.71.13.4:/etc/openvpn/ca.crt "C:\Program Files\OpenVPN\config"
+pscp.exe -pw Miau123 root@10.71.13.4:/etc/openvpn/server/ta.key "C:\Program Files\OpenVPN\config"
